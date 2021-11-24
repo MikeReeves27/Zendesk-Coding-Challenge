232da0f0c3d783851c58af46349371e7b7aa0760
Michael Reeves <62658283+MikeReeves27@users.noreply.github.com>
2021-11-23 20:48:35 -0500

    Update and rename main.py to zendesk_tickets.py
    - Refactored code to allow for unit tests
    - Added minor improvements to functionality and display

---
+++ zendesk_tickets.py
@@ -0,0 +1,198 @@
+import requests
+import getpass
+
+
+# Login funtion:
+def login():
+    '''Return login credentials'''
+
+    # Prompt user to enter their username and password
+    username = input('Please enter your username: ')
+    password = getpass.getpass(prompt = 'Please enter your password (input hidden for privacy): ')
+    credentials = username, password
+    return credentials
+
+
+# Download ticket function:
+def download_tickets(credentials):
+    '''Downloads all tickets from json file and stores them in a list of dictionaries. Return list'''
+    
+    # Start new session and assign login credentials
+    session = requests.Session()
+    session.auth = credentials
+
+    # Set subdomain
+    zendesk = 'https://zccmichaelreeves.zendesk.com'
+
+    # Declare url for tickets.json file
+    url = f'{zendesk}/api/v2/tickets.json'
+
+    # Declare empty list for storing all ticket values
+    ticket_list = []
+
+    # Run loop to retrieve all tickets in the subdomain
+    while url:
+
+        # Run validation on subdomain session. If any code other than 200 is returned,
+        # display error code, prompt user to log in again, and exit program
+        response = session.get(url)
+        if response.status_code != 200:
+            print(f'Error {response.status_code}: {response.reason}')
+            print('Sorry, we are unable to connect to the Ticket Viewer at this time')
+            print('Please check your login credentials and try again')
+            exit()
+
+        # Store json contents in nested dictionary and then store 'ticket' dictionary in list.
+        # Set url to next page in the paginated json file
+        data = response.json()
+        ticket_list.extend(data['tickets'])
+        url = data['next_page']
+
+    return ticket_list
+
+
+# Menu selection input function:
+def menu_selection():
+    '''Returns user's input for menu selection'''
+    user_selection = input('Selection: ')
+    print()
+    return user_selection
+
+
+# Display ticket function
+def display_all_tickets(ticket_list):
+    ''' Displays all tickets from the json file'''
+
+    # Set ticket_index to 0
+    ticket_index = 0;
+
+    # Run loop for as long as the ticket_index is less than the number of total tickets in the list
+    while ticket_index < len(ticket_list):
+
+        # Print ticket title header
+        print()
+        print('TICKET #\tPRIORITY\tSTATUS\t\tTIME CREATED\t\t\tREQUESTER ID\t\tSUBJECT')
+        print('--------\t--------\t------\t\t------------\t\t\t------------\t\t-------')
+
+        # Run loop in 25-ticket increments to keep in line with pagination style guidelines (25 tickets per page)
+        for ticket in range(ticket_index, ticket_index + 25):
+
+            # Print the contents of the ticket, namely: 'id' (ticket ID), 'priority' (ticket priority), 'status' (current status), 
+            # 'created_at' (date/time of ticket creation), 'requester_id' (ID of the user who submitted the ticket), 'subject' (subject/title of the ticket)
+            print(str(ticket_list[ticket]['id']) + '\t\t' + str(ticket_list[ticket]['priority']) + '\t\t' + ticket_list[ticket]['status'] + \
+                  '\t\t' + ticket_list[ticket]['created_at'] + '\t\t' + str(ticket_list[ticket]['requester_id']) + '\t\t' + ticket_list[ticket]['subject'])
+
+        # Increment ticket_index by 25
+        ticket_index += 25
+
+        # After printing each page, print the number of remaining tickets and prompt the user if they want to print next page
+        if ticket_index < len(ticket_list):
+            print()
+            tickets_remaining = len(ticket_list) - ticket_index
+            continue_printing = input(f'There are {tickets_remaining} records remaining in this list. Display next page? (Y/N)\n').lower()
+
+            # Run loop to perform validation on user input
+            while True:
+
+                # If user has entered a 'y' or an 'n', validation is complete
+                if continue_printing == 'y' or continue_printing == 'n':
+                    break
+
+                # If not, prompt user to enter input again
+                continue_printing = input('Invalid input! Please type Y or N\n').lower()
+
+            # If user entered a 'y', continue to next page of ticket_list. Otherwise, return to main menu
+            if continue_printing == 'y':
+                continue
+            else:
+                break
+
+    # Display message saying how many tickets were printed in total
+    print()
+    print(f'Successfully displayed {ticket_index} of {len(ticket_list)} records')
+
+
+# Search for ticket function:
+def search_for_ticket(ticket_list):
+    '''Searches for an individual ticket number from user input'''
+
+    # Prompt user to enter a ticket number
+    ticket_number = input('Please enter a ticket number:\n')
+
+    # Perform input validation. If input is a number, cast it as an integer so that it can match an actual ticket number's data type
+    if ticket_number.isdigit():
+        ticket_number = int(ticket_number)
+
+    # Otherwise, print error message and return to main menu
+    else:
+        print('Invalid input! Ticket number must be numeric')
+        return
+
+    # Set a boolean for if the ticket exists in the list. Set to False by default
+    ticket_exists = False
+
+    # Run loop to check each ticket in the ticket_list
+    for ticket in ticket_list:
+
+        # If the ticket number exists in the list and is paired with a ticket id (that is, the value the user inputs is not just 
+        # coincidentally a matching value for a different key), print the relevent fields. Set ticket_exists boolean to true and break loop
+        if ('id', ticket_number) in ticket.items():
+            print()
+            print('TICKET #\tPRIORITY\tSTATUS\t\tTIME CREATED\t\t\tREQUESTER ID\t\tSUBJECT')
+            print('--------\t--------\t------\t\t------------\t\t\t------------\t\t-------')
+            print(str(ticket['id']) + '\t\t' + str(ticket['priority']) + '\t\t' + ticket['status'] + '\t\t' + ticket['created_at'] + \
+                  '\t\t' + str(ticket['requester_id']) + '\t\t' + ticket['subject'])
+            ticket_exists = True
+            break
+
+    # If the ticket is not in the list, print message for user
+    if not ticket_exists:
+        print('Ticket number not found')
+
+
+# Main function
+def main():
+    '''Main function body'''
+
+    # Run login() function to grab user credentials
+    credentials = login()
+
+    # Pass user credentials into download_tickets() function
+    ticket_list = download_tickets(credentials)
+
+    # Run loop so that user can keep making menu selections until they exit program
+    print('Welcome to the Ticket Viewer, v1.0')
+    while True:
+
+        # Print menu selection options
+        print()
+        print('Please make a selection from the menu:')
+        print('-- Type 1 to view all tickets')
+        print('-- Type 2 to search for a ticket')
+        print('-- Type 3 to exit the Ticket Viewer')
+        print()
+
+        # Run menu_selection() function to get user input
+        user_selection = menu_selection()
+
+        # If user enters 1, display all tickets
+        if user_selection == '1':
+            display_all_tickets(ticket_list)
+
+        # If user enters 2, search for a ticket
+        elif user_selection == '2':
+            search_for_ticket(ticket_list)
+
+        # If user enters 3, exit program
+        elif user_selection == '3':
+            print('Thank you for using the Ticket Viewer!')
+            exit()
+
+        # If user enters anything else, display error and run loop again
+        else:
+            print('Invalid selection! Please type 1, 2, or 3')
+
+
+# Run main function
+if __name__ == "__main__":
+    main()
